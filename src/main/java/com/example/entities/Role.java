0b8bf18e5fd95fc0e08f20a34f486ce18bf85584
Selvakumar E <selvakumar@gmail.com>
2025-04-08 23:14:41 +0530

    Food App - Adding Entity Lombok Annotations

---
+++ src/main/java/com/example/entities/Role.java
@@ -0,0 +1,27 @@
+package com.example.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "role")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "role_name",nullable = false)
+    private String roleName;
+
+    @Column(name = "is_active")
+    private Boolean isActive;
+
+}
