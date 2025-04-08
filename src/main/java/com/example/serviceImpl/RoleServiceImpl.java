20683a356db8cb4cfe6a5be1361fe5914191aa1e
Selvakumar E <selvakumarelango2003@gmail.com>
2025-04-08 23:36:26 +0530

    Food App - Adding Role-repos & serice

---
+++ src/main/java/com/example/serviceImpl/RoleServiceImpl.java
@@ -0,0 +1,15 @@
+package com.example.serviceImpl;
+
+import com.example.repositry.RoleRepository;
+import com.example.service.RoleService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+}
